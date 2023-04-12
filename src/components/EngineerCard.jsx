4e1c2fca6c6577dcbcaf720be57b44b92d86b1c7
Sam Henderson <shendo87@gmail.com>
2023-04-11 20:07:05 -0700

    Complete final layout; missing links and team pictures

--- src/components/EngineerCard.jsx
+++ src/components/EngineerCard.jsx
@@ -2,12 +2,23 @@ import React from "react";
 import ghLogo from '../assets/logo-github.svg'
 import liLogo from '../assets/logo-linkedin.svg'
 
-const EngineerCard = () => {
+const EngineerCard = ({ name, title, pic, gitHubLink, linkedInLink }) => {
 
   return (
-    <div className='engineer-info'>
-
-
+    <div className='engineer-card'>
+      <img class='profile-pic'src={pic} alt="" />
+      <div class='profile-info'>
+        <h5>{name}</h5>
+        <p>{title}</p>
+        <div className='download-links'>
+          <a href={linkedInLink}>
+            <img className="logo" src={liLogo} alt="" />
+          </a>
+          <a href={gitHubLink}>
+            <img className="logo" src={ghLogo} alt="" />
+          </a>
+        </div>
+      </div>
     </div>
   )
 }
--- src/containers/Team.jsx
+++ src/containers/Team.jsx
@@ -1,19 +1,43 @@
 import React from "react";
 import EngineerCard from "../components/EngineerCard";
+import samh from '../assets/samhenderson.jpeg'
 
 
 const Team = () => {
 
   return (
     <div id='team-info'>
-
-      <EngineerCard
-        name='Sam Henderson'
-        title='Software Engineer'
-        pic=''
-        gitHubLink=''
-        linkedInLink=''
+      <h2>OUR TEAM</h2>
+      <div className='engineer-card-cont'>
+        <EngineerCard
+          name='Tricia Yeh'
+          title='Software Engineer'
+          pic=''
+          gitHubLink='https://github.com/triciacorwin'
+          linkedInLink=''
         />
+        <EngineerCard
+          name='Alex Klein'
+          title='Software Engineer'
+          pic=''
+          gitHubLink='https://github.com/a-t-klein'
+          linkedInLink=''
+        />
+        <EngineerCard
+          name='Erika Jung'
+          title='Software Engineer'
+          pic=''
+          gitHubLink='https://github.com/erikahjung'
+          linkedInLink=''
+        />
+        <EngineerCard
+          name='Sam Henderson'
+          title='Software Engineer'
+          pic={samh}
+          gitHubLink='https://github.com/samhhenderson'
+          linkedInLink='https://www.linkedin.com/in/samuel-h-henderson/'
+          />
+      </div>
     </div>
   )
 }
